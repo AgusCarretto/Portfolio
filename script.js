49297e9e7fa9ec5a65ef6618902c36ed5ea5b604
Agustin Carretto <agus.carretto@gmail.com>
2025-01-14 17:31:53 -0300

    1erDia
    Lo que fue el primer dia, decision de sections y movimiento en la pagina

---
+++ script.js
@@ -0,0 +1,12 @@
+// let nav = document.querySelector("#nav");
+// nav.style.display = 'none';
+
+// document.querySelector("#menuBurguer").addEventListener("click", mostrarOcultarMenu);
+
+// function mostrarOcultarMenu () {
+//     if(nav.style.display == 'none')
+//         nav.style.display = 'block';
+//     else
+//         nav.style.display = 'none';
+    
+// }
